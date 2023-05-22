7fe8d6b3379ce4a7ad1ba069e5727d1ca279c28b
dm911 <dmukhatyar82@gmail.com>
2023-05-22 17:51:34 -0400

    removed comments and updated gitignore

--- src/App.js
+++ src/App.js
@@ -4,9 +4,7 @@ import Nav from './components/NavBar';
 import Header from './components/Hero';
 import About from './components/About';
 import Experience from './components/Experience';
-// import Projects from './components/Projects';
 import Contact from './components/Contact';
-// import Footer from './components/Footer';
 
 function App() {
   // Available Colours:
